5eaa631df785eaca0ff223f7baa4ea64e898e641
mahdi-aouchiche <mahdiaouchiche2022@gmail.com>
2022-07-08 14:16:07 -0700

    update previous coppied files for better testing separate labs

---
+++ header/lab3_tests.hpp
@@ -0,0 +1,13 @@
+#ifndef __LAB3_TESTS_HPP__
+#define __LAB3_TESTS_HPP__
+
+/* add all the test files to run in lab 3 */
+#include "op_test.hpp"
+#include "rand_test.hpp"
+#include "add_test.hpp"
+#include "sub_test.hpp"
+#include "mult_test.hpp"
+#include "div_test.hpp"
+#include "pow_test.hpp"
+
+#endif  //__LAB3_TESTS_HPP__
