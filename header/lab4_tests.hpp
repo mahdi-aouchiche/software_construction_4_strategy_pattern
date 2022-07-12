231c9b075f1426485852cf2851630c6e9ffb9367
mahdi-aouchiche <mahdiaouchiche2022@gmail.com>
2022-07-12 14:10:18 -0700

    lab4-strategy patern unit tests

---
+++ header/lab4_tests.hpp
@@ -0,0 +1,10 @@
+#ifndef __LAB4_TESTS_HPP__
+#define __LAB4_TESTS_HPP__
+
+/* add all the test files to run in lab 4 */
+#include "../header/vector_container_test.hpp"
+#include "../header/list_container_test.hpp"
+#include "../header/selection_sort_test.hpp"
+#include "../header/bubble_sort_test.hpp"
+
+#endif  //__LAB4_TESTS_HPP__
---
+++ src/lab4_unit_tests.cpp
@@ -0,0 +1,7 @@
+#include "../header/lab4_tests.hpp"
+#include "../src/container.cpp"
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
